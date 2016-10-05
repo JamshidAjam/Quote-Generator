3b4ef825168be1df445482d987b35f163f6d66f7
Jamshid <mr.jamshid.a@gmail.com>
2016-10-05 21:05:30 +0330

    added quotes

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
 var quotes = [
-	'1111111', 
-	'2222222', 
-	'3333333', 
-	'4444444', 
-	'5555555', 
-	'6666666', 
-	'7777777', 
-	'8888888', 
-	'9999999', 
-	'0000000' 
+	'Solitude is the place of purification.<br>Martin Buber', 
+	'Once you eliminate the impossible, whatever remains, no matter how improbable, must be the truth.<br>Arthur Conan Doyle', 
+	'A goal is a dream with a deadline.<br>Napoleon Hill', 
+	'Art is the stored honey of the human soul, gathered on wings of misery and travail.<br>Theodore Dreiser', 
+	'We waste time looking for the perfect lover, instead of creating the perfect love.<br>Tom Robbins', 
+	'Success is simple. Do what\'s right, the right way, at the right time.<br>Arnold H. Glasow', 
+	'A real friend is one who walks in when the rest of the world walks out.<br>Walter Winchell', 
+	'Life is a dream for the wise, a game for the fool, a comedy for the rich, a tragedy for the poor.<br>Sholom Aleichem', 
+	'Do not take life too seriously. You will never get out of it alive.<br>Elbert Hubbard', 
+	'What you do today can improve all your tomorrows.<br>Ralph Marston',
+	'Every block of stone has a statue inside it and it is the task of the sculptor to discover it.<br>Michelangelo',
+	'A creative man is motivated by the desire to achieve, not by the desire to beat others.<br>Ayn Rand'
 ];
 
 
