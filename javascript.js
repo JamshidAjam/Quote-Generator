508f359f64f2628e5cd0af6b721264224466cebf
Jamshid <mr.jamshid.a@gmail.com>
2016-10-14 12:08:22 +0330

    added several quotes

--- javascript.js
+++ javascript.js
@@ -10,7 +10,16 @@ var quotes = [
 	'Do not take life too seriously. You will never get out of it alive.<br>Elbert Hubbard', 
 	'What you do today can improve all your tomorrows.<br>Ralph Marston',
 	'Every block of stone has a statue inside it and it is the task of the sculptor to discover it.<br>Michelangelo',
-	'A creative man is motivated by the desire to achieve, not by the desire to beat others.<br>Ayn Rand'
+	'A creative man is motivated by the desire to achieve, not by the desire to beat others.<br>Ayn Rand',
+	'It alweys seems imposibble until it\'s done<br>Nelson Mandela',
+	'Alweys do your best. What you plant now, you will harvest later.<br>Og Mandino',
+	'It\'s not whether you get knocked down, it\'s whether you get back up.',
+	'The harder the conflict, the more glorious the triumph.<br>Thomas Paine',
+	'In order to succeed, we must first believe that we can.<br>Nikos Kazantzakis',
+	'What you do today can improve all your tomorrows.<br>Ralph Marston',
+	'You will never win if you never begin.<br>Helen Rowland',
+	'It does not matter how slowly you go as long as you do not stop.<br>Confucius',
+	'You just can\'t beat the person who never gives up.<br>Babe Ruth'
 ];
 
 
